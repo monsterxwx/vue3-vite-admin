755963300ea906b31f9c4f706172bbc30828e41c
xwx623989195 <623989195@qq.com>
2023-10-26 14:58:05 +0800

    feat: 增加环境变量

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx' // 引入jsx
@@ -10,39 +10,45 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // 自动导入svg图标
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx({
-    // 配置选项
-    }),
-    Unocss(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    }),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]'
-    })
-  ],
-  resolve: {
-    extensions: ['.vue', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
-  server: {
-    host: '0.0.0.0'
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/styles/variables.scss";'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      vueJsx({
+      // 配置选项
+      }),
+      Unocss(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]'
+      })
+    ],
+    resolve: {
+      extensions: ['.vue', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+      alias: {
+        '@': path.join(__dirname, 'src')
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: env.VITE_PORT,
+      cors: true
+    },
+    base: env.VITE_PUBLIC_PATH,
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/styles/variables.scss";'
+        }
       }
     }
   }
