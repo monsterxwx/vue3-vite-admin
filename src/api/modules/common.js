58bdb71c7b90bcafd4a1f9c666bd4c6960f40817
coderxwx <623989195@qq.com>
2025-11-19 16:41:25 +0800

    build: 1

--- src/api/modules/common.js
+++
@@ -1,7 +0,0 @@
-import { get, post } from '../api'
-
-export default {
-  // 登录
-  getTianqi: (data) => get('/api', data),
-  logined: (data) => post('/login', data)
-}
--- src/hooks/common/useMessage.js
+++
@@ -1,9 +0,0 @@
-import { ElMessage } from 'element-plus'
-
-export default function useMessage (type = 'success', message) {
-  ElMessage({
-    message,
-    type, // "success" | "warning" | "info" | "error"
-    duration: 2000
-  })
-}
