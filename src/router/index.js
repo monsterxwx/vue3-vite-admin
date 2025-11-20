ce6f4341e817a1ebaa084ea4e29556ad3f15704e
coderxwx <623989195@qq.com>
2025-11-20 08:51:42 +0800

    feat: 修改路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import NProgress from '@/utils/nprogress'
 
 const routes = [
@@ -77,7 +77,7 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHashHistory()
+  history: createWebHistory()
 })
 
 router.beforeEach((to, from, next) => {
