d5497854219acea5f46d6dd2406025054e8298e5
coderxwx <623989195@qq.com>
2025-11-20 09:06:10 +0800

    feat: 修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import NProgress from '@/utils/nprogress'
 
 const routes = [
@@ -77,7 +77,7 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHistory()
+  history: createWebHashHistory()
 })
 
 router.beforeEach((to, from, next) => {
